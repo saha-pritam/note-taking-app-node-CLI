cc5d05c05d674b674fa31453612e7be51bce20a5
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-27 09:30:53 +0530

    All the four commands are added, but with a demo handler just to check if everything is working fine.

---
+++ index.js
@@ -0,0 +1,65 @@
+const yargs = require("yargs");
+
+yargs.command({
+    command:'add',
+    describe:'It will add a new note to the existing notes file. If a note with duplicate title is tried to add then it will dispay in red that title already taken else show success message in green.',
+    builder:{
+        title:{
+            describe:'For title of the note',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'For body of the note',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:(argv)=>{
+        console.log('Add command is working')
+        console.log('Title - ',argv.title)
+        console.log('Body - ',argv.body)
+    }
+})
+
+yargs.command({
+    command:'delete',
+    describe:'It will delete the note matching with the title. If title is not matching it will display in red title not found else, it will display success message in green.',
+    builder:{
+        title:{
+            describe:'For title of the note',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:(argv)=>{
+        console.log('Delete command is working')
+        console.log('Title - ',argv.title)
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'It will read the note matching with the title. If title is not matching it will display in red title not found else, it will display a success message in green and then both the title and body in white.',
+    builder:{
+        title:{
+            describe:'For title of the note',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:(argv)=>{
+        console.log('Read command is working')
+        console.log('Title - ',argv.title)
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'It will show all the notes (title and body) with a success message in green if present otherwise display empty list in red.',
+    handler:()=>{
+        console.log('List command is working')
+    }
+})
+
+yargs.parse()
