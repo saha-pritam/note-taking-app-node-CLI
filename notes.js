8f0e70935848efaac1b8830c2f8a2ba33270ce3a
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-27 10:06:53 +0530

    Modified getNotes to handle totally empty notes.json

--- notes.js
+++ notes.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 
+//This method will return all the notes from notes.json file if present in an array of objects form else it will return empty array in case the notes.json file contains empty json or the file doesn't exist or the notes.json file is totally empty.
 const getNotes=()=>{
-    if(!fs.existsSync('notes.json'))
+    try {
+        if (!fs.existsSync('notes.json'))
+            return []
+        return JSON.parse(fs.readFileSync('notes.json').toString())
+    }
+    catch (e) {
         return []
-    return JSON.parse(fs.readFileSync('notes.json').toString())
+    }
 }
 
+//This method will save data to notes.json by deleting existing contents from notes.json everytime.
 const saveNotes=(data)=>{
     data = JSON.stringify(data)
     fs.writeFileSync('notes.json',data)
 }
-
